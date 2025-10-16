efda2af2c273d89948a51f3eb1168bc53e5afb52
Sai Ganesh Darni <saiganeshdarni@gmail.com>
2025-10-17 00:28:17 +0530

    Added basic signup view

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from products.views import home_view
+from users.views import signup_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('signup', signup_view, name='signup'),
     path('', home_view, name='home'),
     path('products/', include('products.urls')),
 ]
